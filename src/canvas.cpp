ea8898b602d6e7b4fba8313c1f8af36ed4e105b1
R0hero <benjaminelsholm@gmail.com>
2026-09-06 18:08:37 +0200

    fix: move canvas into correct folder

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,9 +1,14 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <image_generator/color.hpp>
 
 class Canvas {
     public:
         Canvas(int width, int height);
+        // member functions
+        void setPixel(int x, int y, struct color) {
+
+        }
     private:
         int width_, height;
 };
