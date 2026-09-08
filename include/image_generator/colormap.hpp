99c1be33cb746987cdaa37777404d1e8443c5df7
R0hero <benjaminelsholm@gmail.com>
2026-09-08 20:34:09 +0200

    feat: add constructor for Colormap

---
+++ include/image_generator/colormap.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "image_generator/color.hpp"
+#include <vector>
+
+class Colormap {
+    public:
+        Colormap(const std::vector<Color>& colors);
+        // member functions
+        Color sample(double t) const;
+    private:
+        std::vector<Color> colors;
+};
