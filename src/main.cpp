c2d0837a53f9daa9015acb2833c4fd4b5ff7ea9d
R0hero <benjaminelsholm@gmail.com>
2026-09-08 20:33:32 +0200

    test: introduce lerpColor in gradient

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "image_generator/canvas.hpp"
 #include "image_generator/color.hpp"
 #include "image_generator/utils/math_utils.hpp"
+#include "image_generator/utils/color_utils.hpp"
 
 int main() {
     int width = 500;
@@ -11,7 +12,10 @@ int main() {
 
     for (int x = 0; x <= width-1; x++) {
         for (int y = 0; y <= height-1; y++) {
-            Color c(0, normalizeToRange(x, 0, width, 0, 255), normalizeToRange(y, 0, height, 0, 255));
+            // Color c(0, normalizeToRange(x, 0, width, 0, 255), normalizeToRange(y, 0, height, 0, 255));
+            Color a(0, normalizeToRange(x, 0, width, 0, 255), 0);
+            Color b(normalizeToRange(x, 0, width, 0, 255), 0, normalizeToRange(y, 0, height, 0, 255));
+            Color c = lerpColor(a, b, 0.4);
             canvas.setPixel(x, y, c);
         };
     };
