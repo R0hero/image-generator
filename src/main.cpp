98388acb57b5e55997de06c947f42d4ced0a3ee2
R0hero <benjaminelsholm@gmail.com>
2026-09-09 19:59:43 +0200

    test: replace current logic with test of colormap

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,25 @@ int main() {
 
     for (int x = 0; x <= width-1; x++) {
         for (int y = 0; y <= height-1; y++) {
+            // test for if canvas is being filled with the same colors no matte the size of the canvas
             // Color c(0, normalizeToRange(x, 0, width, 0, 255), normalizeToRange(y, 0, height, 0, 255));
-            Color a(0, normalizeToRange(x, 0, width, 0, 255), 0);
-            Color b(normalizeToRange(x, 0, width, 0, 255), 0, normalizeToRange(y, 0, height, 0, 255));
-            Color c = lerpColor(a, b, 0.4);
-            canvas.setPixel(x, y, c);
+            
+            // test for if the interpolation in lerpColor works as expected
+            // Color a(0, normalizeToRange(x, 0, width, 0, 255), 0);
+            // Color b(normalizeToRange(x, 0, width, 0, 255), 0, normalizeToRange(y, 0, height, 0, 255));
+            // Color c = lerpColor(a, b, 0.4);
+            
+            double t = static_cast<double>(x) / (width - 1);
+            Color a(253, 231, 37);
+            Color b(94, 201, 98);
+            Color c(33, 145, 140);
+            Color d(59, 82, 139);
+            Color e(68, 1, 84);
+            std::vector<Color> colors = {a, b, c, d, e};
+            Colormap colormap(colors);
+            Color sampled_color = colormap.sample(t);
+
+            canvas.setPixel(x, y, sampled_color);
         };
     };
 
