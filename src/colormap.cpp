0940b6682ecb57f5e7f4e62648dd2513df164d90
R0hero <benjaminelsholm@gmail.com>
2026-09-09 19:26:24 +0200

    feat: handle edgecase when t=1.0

--- src/colormap.cpp
+++ src/colormap.cpp
@@ -9,6 +9,7 @@ Colormap::Colormap(const std::vector<Color>& colors) : colors_(colors) {
 Color Colormap::sample(double t) const {
     int sections = colors_.size() - 1;
     int segment_index = static_cast<int>(t * sections);
+    segment_index = std::min(segment_index, sections - 1);
     double local_t = t * sections - segment_index;
 
     Color color = lerpColor(colors_[segment_index], colors_[segment_index+1], local_t);
