33297f8b2de8bbbd9d45f68d1f478dbe32002477
R0hero <benjaminelsholm@gmail.com>
2026-09-09 19:32:20 +0200

    feat: include algorithm for std::min

--- src/colormap.cpp
+++ src/colormap.cpp
@@ -1,6 +1,7 @@
 #include "image_generator/colormap.hpp"
 #include "image_generator/utils/color_utils.hpp"
 #include <vector>
+#include <algorithm>
 
 Colormap::Colormap(const std::vector<Color>& colors) : colors_(colors) {
 
