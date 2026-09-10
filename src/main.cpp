21e8be871a97abf422558c7e401cbd549402868b
R0hero <benjaminelsholm@gmail.com>
2026-09-10 11:06:23 +0200

    style: deleted old commented-out testing lines in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,6 @@ int main() {
 
     for (int x = 0; x <= width-1; x++) {
         for (int y = 0; y <= height-1; y++) {
-            // test for if canvas is being filled with the same colors no matte the size of the canvas
-            // Color c(0, normalizeToRange(x, 0, width, 0, 255), normalizeToRange(y, 0, height, 0, 255));
-            
-            // test for if the interpolation in lerpColor works as expected
-            // Color a(0, normalizeToRange(x, 0, width, 0, 255), 0);
-            // Color b(normalizeToRange(x, 0, width, 0, 255), 0, normalizeToRange(y, 0, height, 0, 255));
-            // Color c = lerpColor(a, b, 0.4);
-            
             double t = static_cast<double>(x) / (width - 1);
             Color sampled_color = colormap.sample(t);
 
